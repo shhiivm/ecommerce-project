efd1e08b472f9f24ea314a9481d9825113aa86d3
shhiivm <shhiivm@pm.me>
2025-08-27 22:12:44 +0530

    use axios to fetch the data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import "../style/pages/HomePage.css";
 import { Header } from "../components/Header";
 import { products } from "../../starting code/data/products";
+import axios from 'axios';
+
 const HomePage = () => {
-  fetch("http://localhost:3000/api/products").then((response) => {
-    return response.json();
-  }).then((data) => {
-      console.log(data);
-    });
+  axios.get("http://localhost:3000/api/products").then((response) => {
+    console.log(response.data);
+  })
   return (
     <>
       <Header />
