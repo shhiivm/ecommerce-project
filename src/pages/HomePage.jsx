d0118edf37d5f7be9aac37289db06e97b799754d
shhiivm <shhiivm@pm.me>
2025-08-30 22:19:34 +0530

    use data from backend to generate products

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import "../style/pages/HomePage.css";
 import { Header } from "../components/Header";
-import { products } from "../../starting code/data/products";
-import axios from 'axios';
+// import { products } from "../../starting code/data/products";
+import axios from "axios";
 
 const HomePage = () => {
-  axios.get("http://localhost:3000/api/products").then((response) => {
-    console.log(response.data);
-  })
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/products").then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
+
   return (
     <>
       <Header />
